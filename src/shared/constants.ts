1e0efc774226e7094211f5b36a3a19b9d9a6a0bf
aztecian batman <aztecianbatman@gmail.com>
2026-09-05 10:21:04 +0530

    feat: add service endpoints and navigation constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,4 @@
+export const MOJANG_VERSION_MANIFEST="https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
+export const MODRINTH_API="https://api.modrinth.com/v2";
+export const APP_VERSION="0.1.0";
+export const NAV_ITEMS=["Home","Instances","Discover","Modpacks","Mods","Versions","Worlds","Downloads","Accounts","Settings"] as const;
